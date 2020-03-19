65a7d80dafba57754066e297490382b2c2e6e5cf
thedarkkid <thedarkkid.codes@gmail.com>
2020-03-19 12:26:53 +0400

    add delete functionality

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+function classOnClick(className, fn) {
+    let nodes = document.getElementsByClassName(className);
+    for (let i = 0; i < nodes.length; i++) {
+        let node = nodes[i];
+        node.addEventListener("click", function (e) { fn(e) });
+    }
+}
